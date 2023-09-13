ee38856e33fcfe838d0b7457c9d51d55acf0bead
hurali97 <hurali97@gmail.com>
2023-09-13 22:03:45 +0500

    refactor: bypass user concern on local

--- scripts/modify_podfile.rb
+++ scripts/modify_podfile.rb
@@ -4,18 +4,27 @@ require_relative './utils'
 
 isLocalEnv = Dir.exist?('example')
 
-puts "Do you want to automatically update the Podfile? (Y/N)"
-response = gets.chomp.downcase
-
-if response == 'y'
+def update_files(isLocalEnv)
   update_conversions(isLocalEnv)
   update_graphics_conversions(isLocalEnv)
 
   warn("\nAdding TodayWidgetExtension target & dependencies to the Podfile\n")
   update_podfile(isLocalEnv)
-elsif response == 'n'
-  update_conversions(isLocalEnv)
-  update_graphics_conversions(isLocalEnv)
+end
+
+
+if isLocalEnv == false
+  puts "Do you want to automatically update the Podfile? (Y/N)"
+  response = gets.chomp.downcase
+
+  if response == 'y'
+    update_files(isLocalEnv)
+  elsif response == 'n'
+    update_conversions(isLocalEnv)
+    update_graphics_conversions(isLocalEnv)
+  else
+    info("Invalid response. Please enter Y or N.")
+  end
 else
-  info("Invalid response. Please enter Y or N.")
+  update_files(isLocalEnv)
 end
