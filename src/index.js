0b066e139d57d829993457d45f62665e32b66525
Muhammad Hur Ali <hurali97@gmail.com>
2023-11-15 23:17:38 +0500

    refactor: remove scrollview and textinput component (#5)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import Circle from './js/Circle';
 import Mask from './js/Mask';
 import Rect from './js/Rect';
 import Shadow from './js/Shadow';
-import ScrollView from './js/ScrollView';
 import Image from './js/Image';
 import Animation from './js/Animation';
 import Blur from './js/Blur';
@@ -15,7 +14,6 @@ export {
   Mask,
   Rect,
   Shadow,
-  ScrollView,
   Image,
   Animation,
   Blur,
